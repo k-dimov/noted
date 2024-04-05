6f23410f838203a880272423ba5a69b9e7f86355
k-dimov <kaloyan.dimov@hotmail.com>
2024-04-05 18:30:14 +0300

    login and register forms added

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+	registerHandler(form:NgForm) {
+		console.log(form.valid)
+	}
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,17 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
-
-
-
+import { UserRoutingModule } from './user-routing.module';
+import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+    imports: [CommonModule, RouterModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+    exports: [UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
