7c7e11650a99c0d72eecd9fcf08aaf944d5be343
k-dimov <kaloyan.dimov@hotmail.com>
2024-04-05 18:58:41 +0300

    firebase authentication added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { environment } from 'src/environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,19 +13,18 @@ import { HomeComponent } from './home/home.component';
 import { UserRoutingModule } from './user/user-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CoreModule,
-    UserModule,
-    UserRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomeComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CoreModule,
+        UserModule,
+        UserRoutingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBNYiaVsWyGIuX4Vk112j-fo5ghgN_WuvY",
-  authDomain: "angular-project-7a0a7.firebaseapp.com",
-  databaseURL: "https://angular-project-7a0a7-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "angular-project-7a0a7",
-  storageBucket: "angular-project-7a0a7.appspot.com",
-  messagingSenderId: "366269520323",
-  appId: "1:366269520323:web:078afc942c5f1dde1e0576",
-  measurementId: "G-489Z7T3X4S"
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyBNYiaVsWyGIuX4Vk112j-fo5ghgN_WuvY",
+		authDomain: "angular-project-7a0a7.firebaseapp.com",
+		databaseURL: "https://angular-project-7a0a7-default-rtdb.europe-west1.firebasedatabase.app",
+		projectId: "angular-project-7a0a7",
+		storageBucket: "angular-project-7a0a7.appspot.com",
+		messagingSenderId: "366269520323",
+		appId: "1:366269520323:web:078afc942c5f1dde1e0576",
+		measurementId: "G-489Z7T3X4S"
+	  }
+}
